9f32ffb195c6067d72df2d776b1f3239c2d83560
Aguinaldo Martins <102426405+manoguinas@users.noreply.github.com>
2025-02-04 15:19:59 -0300

    Add files via upload

---
+++ aula12/ValidarApplication.java
@@ -0,0 +1,13 @@
+package com.validacao.validar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidarApplication.class, args);
+	}
+
+}
